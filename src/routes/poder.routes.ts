a3477c828ce89ecf70bd8acf81b6f86bcd0ca35f
enzo <e.ferrari449@gmail.com>
2025-07-07 20:15:49 -0300

    CRUD DE SOLICITUD ACTUALIZADO SIGUIENDO PRINCIPIOS SOLID

--- src/routes/poder.routes.ts
+++ src/routes/poder.routes.ts
@@ -1,4 +1,25 @@
 import { Router } from "express";
+import { PoderController } from "../controller/poder.controller.js";
+import { PoderService } from "../services/poder.service.js";
+import { InMemoryPoderRepository } from "../repositories/in-memory-poder.repository.js";
+
+const router = Router();
+
+const repository = new InMemoryPoderRepository();
+const service = new PoderService(repository);
+const controller = new PoderController(service);
+
+// Define las rutas para los poderes
+router.get("/", controller.getPoderes);
+router.get("/:id", controller.getPoder);
+router.post("/", controller.createPoder);
+router.put("/:id", controller.updatePoder);
+router.delete("/:id", controller.deletePoder);
+
+export default router;
+
+
+/*import { Router } from "express";
 import {
     getPoderes,
     getPoder,
@@ -16,3 +37,4 @@ router.put("/:id", updatePoder);
 router.delete("/:id", deletePoder);
 
 export default router;
+*/
