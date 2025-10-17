7ef76e642f98003405c38beac74c3f628c0435a9
ddvst <iruiztrabajo@gmail.com>
2025-10-17 07:28:25 -0300

    Cambios necesarios para funcionamiento del frontend. Rutas añadidas para la busqueda de carpetas por parte de metahumano. Controller de carpeta modificado para permitir modificar el estado.

--- src/carpeta/carpeta.routes.ts
+++ src/carpeta/carpeta.routes.ts
@@ -1,12 +1,15 @@
-import {findAll, findOne, LinkCarpBuro, remove, } from "./carpeta.controller.js";
+import {findAll, findOne, LinkCarpBuro, remove, updateEstado, findByMetahumano} from "./carpeta.controller.js";
 import { Router } from 'express'
 import { sanitizeCarpetaInput } from "./carpeta.controller.js"
 
 const carpetaRouter = Router();
 
+carpetaRouter.patch('/:id/estado', sanitizeCarpetaInput, updateEstado);
 carpetaRouter.get('/',findAll)
 carpetaRouter.get('/:id', findOne)
 carpetaRouter.post('/',sanitizeCarpetaInput,LinkCarpBuro)
 carpetaRouter.delete('/:id', remove)
+carpetaRouter.get('/by-metahumano/:metahumanoId', findByMetahumano);
+
 
 export {carpetaRouter}
